086b7bcfee2f060a231dbbcd4422f358d567ba90
layar247 <140887333+layar247@users.noreply.github.com>
2023-10-22 15:58:05 +0300

    Update slick.js

--- slick.js
+++ slick.js
@@ -4,16 +4,16 @@ $(document).ready(function () {
 		dots: true,
 		infinite: true,
 		speed: 200,
-		slidesToShow: 4,
-		slidesToScroll: 4,
+		slidesToShow: 3,
+		slidesToScroll: 3,
 		prevArrow: $('#prev'),
 		nextArrow: $('#next'),
 		responsive: [
 			{
 				breakpoint: 768,
 				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2,
+					slidesToShow: 1,
+					slidesToScroll: 1,
 					infinite: true,
 					dots: true
 				}
